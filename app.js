889dcdc1c2cf4a44a10003e94f70a068a0e3312a
Charles Lane <cglane@Charless-MacBook-Pro.local>
2015-10-26 14:09:05 -0400

    functioning album

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var uniquePhoto;
 var albumCount = 0;
 var albumName;
 var albumNameDiv;
+var photoNumber = 1;
 $(document).ready(function(){
   var numAlbums = 0;
   var photoImages = _.template($('#photoImages').html());
@@ -19,33 +20,48 @@ $(document).ready(function(){
         _.each(el,function(thirdelement){
             uniquePhoto = thirdelement;
             albumNameDiv = "#" + albumName;
-            $(albumNameDiv).append(photoImages(uniquePhoto));
+            $(albumNameDiv).append(photoImages(uniquePhoto,photoNumber));
+            photoNumber ++;
         });
+        //set background image of album div
+
         $('aside button').on('click',function(event){
           event.preventDefault();
           var clickedSelection = "#" + $(this).attr('rel');
+          var clickedSelectionP = "#" + $(this).attr('rel')+ " " + "p";
+
           console.log(clickedSelection);
           var siblings = $('clickedSelection').siblings('section');
           var children = $('section').children();
           console.log(siblings);
-          $(clickedSelection).children().css({"display":"block", "width":"100%"});
-          $(clickedSelection).css({"display":"block  ", "width":"100%"});
+          $(clickedSelection).children().css({"display":"inline-block", "width":"30%"});
+          $(clickedSelectionP).css({"width": "50%", "margin-left":"35%"});
+          $(clickedSelection).css({"display":"inline-block ", "width":"100%"});
           $(clickedSelection).siblings().css({"display":"none  ", "width":"100%"});
 
         });
-        $('section .albumDiv').on('click',function(event){
-          var clickedSelection = "#" + $(this).attr('rel');
-          var siblings = $(this).siblings();
-          console.log(siblings);
+        $('section .albumDiv p').on('click',function(event){
+          var clickedSelection = $(this).closest("div");
           console.log(clickedSelection);
+          var siblings = clickedSelection.siblings();
+          var clickedSelectionP = $(this);
+          console.log(clickedSelectionP);
           event.preventDefault();
           $('aside button').css({"display":"block"});
           $(siblings).css({"display":"none"});
           $(clickedSelection).css({"width":"800px"});
           $(clickedSelection).children().css({"display":"inline-block","width": "30%"});
-          $(clickedSelection).children().children().css({"width":"40%","display":"inline-block"});
+          $(clickedSelectionP).css({"width": "70%", "margin-left":"35%"});
+          $(clickedSelection).children().children().css({"width":"100%","display":"inline-block"});
 
       });
+        $('section .albumDiv .photoDiv').on('click',function(event){
+            var clickedSelection = $(this).attr('id');
+            var photoIdNumber = "#" + clickedSelection;
+            $(photoIdNumber).css({"display": "block", "width": "100%"});
+            $(photoIdNumber).siblings().css({"display":"none", "width": "50"});
+            console.log(photoIdNumber);
+        });
       });
   });
 
